789ea37c798fbd22470f7871ab9ac82a86ce77cc
yi-ting <yumy19990628@gmail.com>
2023-03-18 20:56:26 +0800

    [Add] Chart style

--- src/pages/ChartPage/ChartPage.js
+++ src/pages/ChartPage/ChartPage.js
@@ -1,60 +1,58 @@
+import React from 'react';
 import styled from 'styled-components/macro';
-import ReactLoading from 'react-loading';
-import analysis from './analysis.png';
-import stock from './packages.png';
-import checkList from './checklist.png';
-import user from './user.png';
-import goldMedal from './goldMedal.png';
-import silverMedal from './silverMedal.png';
-import copperMedal from './copperMedal.png';
+import './reset.css';
+import GlobalStyle from './GobalStyle';
+import 'c3/c3.css';
+import c3 from 'c3';
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   width: 100vw;
   min-height: calc(100vh - 115px);
 `;
-
-const SideMenu = styled.div`
-  background-color: bisque;
+/*/////////////left-bar/////////////*/
+const MenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 20%;
-  text-align: center;
+  padding-top: 50px;
+  gap: 30px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
 `;
 
-const SideMenuItem = styled.a`
+const SideMenuList = styled.p`
   display: flex;
-  height: 130px;
-  padding-left: 20px;
   align-items: center;
+  letter-spacing: 1px;
+  width: 150px;
+  margin: 0 auto;
+`;
+const SideMenuListTitle = styled(SideMenuList)`
+  font-weight: 600;
+  font-size: 24px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid black;
+`;
+const Icon = styled.img`
+  margin-right: 10px;
+  width: 30px;
 `;
 
-const SideMenuListImage = styled.img`
-  width: 10%;
-  height: 20%;
-  margin-right: 15px;
-`;
-
-const SideMenuListTitle = styled.p`
-  font-size: 25px;
-`;
-
-const MainContainer = styled.div`
-  width: 1200px;
-  margin: 0 100px;
-`;
-
+/*/////////////Pie/////////////*/
 const ClickRateContainer = styled.ul`
-  display: flex;
-  gap: 38px;
+  display: grid;
   margin-top: 50px;
+  gap: 15px;
+  grid-template-columns: 1fr 1fr;
 `;
 
 const ClickRate = styled.li`
-  width: 25%;
-  height: 30%;
   list-style: none;
   background-color: ${(props) => props.color};
-  border-radius: 20px;
-  padding: 30px;
+  border-radius: 15px;
+  height: 250px;
+  padding: 20px;
 `;
 
 const ClickRateNumber = styled.p`
@@ -69,121 +67,176 @@ const ClickRateTitle = styled.p`
   font-size: 20px;
 `;
 
-const ClickRateImage = styled.img`
-  width: 45px;
-  height: 45px;
-  margin-top: 80px;
-`;
-
-const ThritySalesChartSection = styled.div`
-  width: 1000px;
-  height: 500px;
+/*/////////////Main/////////////*/
+const MainContainer = styled.div`
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  width: 70%;
 `;
 
-const ThritySaleChartTitle = styled.p`
-  font-size: 25px;
+const ChartTitle = styled.p`
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 30px;
+`;
+const ChartImg = styled.div`
   padding: 20px;
 `;
-
-const ThritySalesChart = styled.div`
-  width: 1000px;
-  height: 500px;
-  background-color: lemonchiffon;
+const Timeseries = styled.div``;
+/*/////////////Rank/////////////*/
+const RankItems = styled.div`
+  display: flex;
+  gap: 50px;
+`;
+const Rank = styled.div``;
+const RankItem = styled.p`
+  margin-bottom: 15px;
+`;
+const RankItemTop3 = styled(RankItem)`
+  font-size: 24px;
 `;
 
-const EveryMonthChartSection = styled.div`
-  width: 1000px;
-  height: 500px;
-  margin: 0 auto;
-`;
+/*/////////////Chart/////////////*/
+const Chart = () => {
+  React.useEffect(() => {
+    c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25],
+        ],
+        type: 'line',
+      },
+      size: {
+        height: 300,
+      },
+    });
+  }, []);
 
-const EveryMonthChartTitle = styled.div`
-  font-size: 25px;
-  padding: 20px;
-`;
+  return <div id="chart" />;
+};
 
-const EveryMonthChart = styled.div`
-  width: 1000px;
-  height: 500px;
-  background-color: lemonchiffon;
-`;
+const Chart2 = () => {
+  React.useEffect(() => {
+    c3.generate({
+      bindto: '#chart2',
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25],
+        ],
+        type: 'line',
+      },
+      size: {
+        height: 300,
+      },
+    });
+  }, []);
 
-const CategoryChartSection = styled.ul`
-  background-color: darkorange;
-  width: 500px;
-`;
+  return <div id="chart2" />;
+};
 
-const CategoryChartItem = styled.li``;
+const Chart3 = () => {
+  React.useEffect(() => {
+    c3.generate({
+      bindto: '#chart3',
+      data: {
+        columns: [
+          ['衣服', 10],
+          ['衣服2', 20],
+          ['衣服3', 5],
+        ],
+        type: 'pie',
+        colors: {
+          衣服: '#E68618',
+          衣服2: '#F280CA',
+          衣服3: '#26BFC7',
+        },
+      },
+      size: {
+        height: 200,
+      },
+    });
+  }, []);
 
-const CategoryChartTitle = styled.p``;
-
-const CategoryChart = styled.div``;
+  return <div id="chart3" />;
+};
 
 function ChartPage() {
   return (
-    <Wrapper>
-      <SideMenu>
-        <SideMenuItem>
-          <SideMenuListImage src={analysis} />
-          <SideMenuListTitle>報表總覽</SideMenuListTitle>
-        </SideMenuItem>
-        <SideMenuItem>
-          <SideMenuListImage src={stock} />
-          <SideMenuListTitle>庫存查詢</SideMenuListTitle>
-        </SideMenuItem>
-        <SideMenuItem>
-          <SideMenuListImage src={checkList} />
-          <SideMenuListTitle>訂單資料</SideMenuListTitle>
-        </SideMenuItem>
-      </SideMenu>
-      <MainContainer>
-        <ClickRateContainer>
-          <ClickRate color={`#e6422b`}>
-            <ClickRateNumber color={`#fff`}>100</ClickRateNumber>
-            <ClickRateTitle color={`#fff`}>全站點擊率</ClickRateTitle>
-            <ClickRateImage src={user} />
-          </ClickRate>
-          <ClickRate color={`#EFA35F`}>
-            <ClickRateNumber>50</ClickRateNumber>
-            <ClickRateTitle>第一名點擊率</ClickRateTitle>
-            <ClickRateImage src={goldMedal} />
-          </ClickRate>
-          <ClickRate color={`#F3E3D4`}>
-            <ClickRateNumber>30</ClickRateNumber>
-            <ClickRateTitle>第二名點擊率</ClickRateTitle>
-            <ClickRateImage src={silverMedal} />
-          </ClickRate>
-          <ClickRate color={`#95C6B6`}>
-            <ClickRateNumber color={`#fff`}>10</ClickRateNumber>
-            <ClickRateTitle color={`#fff`}>第三名點擊率</ClickRateTitle>
-            <ClickRateImage src={copperMedal} />
-          </ClickRate>
-        </ClickRateContainer>
-        <ThritySalesChartSection>
-          <ThritySaleChartTitle>30 天銷售金額</ThritySaleChartTitle>
-          <ThritySalesChart></ThritySalesChart>
-        </ThritySalesChartSection>
-        <EveryMonthChartSection>
-          <EveryMonthChartTitle>每月銷售金額</EveryMonthChartTitle>
-          <EveryMonthChart></EveryMonthChart>
-        </EveryMonthChartSection>
-      </MainContainer>
-      <CategoryChartSection>
-        <CategoryChartItem>
-          <CategoryChartTitle>男裝銷售量</CategoryChartTitle>
-          <CategoryChart></CategoryChart>
-        </CategoryChartItem>
-        <CategoryChartItem>
-          <CategoryChartTitle>女裝銷售量</CategoryChartTitle>
-          <CategoryChart></CategoryChart>
-        </CategoryChartItem>
-        <CategoryChartItem>
-          <CategoryChartTitle>配件銷售量</CategoryChartTitle>
-          <CategoryChart></CategoryChart>
-        </CategoryChartItem>
-      </CategoryChartSection>
-    </Wrapper>
+    <>
+      {/* <GlobalStyle /> */}
+      <Wrapper>
+        <MenuContainer>
+          <SideMenuListTitle>Dashboard</SideMenuListTitle>
+          <SideMenuList>
+            <Icon src="https://cdn-icons-png.flaticon.com/512/993/993762.png" />
+            報表總覽
+          </SideMenuList>
+          <SideMenuList>
+            <Icon src="https://cdn-icons-png.flaticon.com/512/4947/4947506.png" />
+            庫存管理
+          </SideMenuList>
+          <SideMenuList>
+            <Icon src="https://cdn-icons-png.flaticon.com/512/839/839860.png" />
+            訂單資料
+          </SideMenuList>
+        </MenuContainer>
+        <MainContainer>
+          <ClickRateContainer>
+            <ClickRate color={`#e6422b`}>
+              <ClickRateNumber color={`#fff`}>100</ClickRateNumber>
+              <ClickRateTitle color={`#fff`}>全站點擊率</ClickRateTitle>
+            </ClickRate>
+            <ClickRate color={`#EFA35F`}>
+              <Chart3 />
+            </ClickRate>
+            <ClickRate color={`#F3E3D4`}></ClickRate>
+            <ClickRate color={`#95C6B6`}></ClickRate>
+          </ClickRateContainer>
+          <Timeseries>
+            <ChartTitle>30 天銷售金額</ChartTitle>
+            <ChartImg>
+              <Chart />
+            </ChartImg>
+          </Timeseries>
+          <Timeseries>
+            <ChartTitle>每月銷售金額</ChartTitle>
+            <ChartImg>
+              <Chart2 />
+            </ChartImg>
+          </Timeseries>
+          <Rank>
+            <ChartTitle>銷售排行</ChartTitle>
+            <RankItems>
+              <div>
+                <RankItemTop3>
+                  <span>TOP1</span> 前開扭結洋裝
+                </RankItemTop3>
+                <RankItemTop3>
+                  <span>TOP2</span> 前開扭結洋裝
+                </RankItemTop3>
+                <RankItemTop3>
+                  <span>TOP3</span> 前開扭結洋裝
+                </RankItemTop3>
+              </div>
+              <div>
+                <RankItem>前開扭結洋裝</RankItem>
+                <RankItem>前開扭結洋裝</RankItem>
+                <RankItem>前開扭結洋裝</RankItem>
+                <RankItem>前開扭結洋裝</RankItem>
+                <RankItem>前開扭結洋裝</RankItem>
+                <RankItem>前開扭結洋裝</RankItem>
+                <RankItem>前開扭結洋裝</RankItem>
+              </div>
+            </RankItems>
+          </Rank>
+        </MainContainer>
+      </Wrapper>
+    </>
   );
 }
 
