7f1854a5b130ed960f5fcdd7394f980992dcf7db
yi-ting <yumy19990628@gmail.com>
2023-03-20 19:10:48 +0800

    [Update] PostPage style

--- src/pages/Blog/Post.js
+++ src/pages/Blog/Post.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import SideMenu from '../../components/SideMenu';
+import bannerImg from './blogImg.png';
+import StockBanner from '../../components/StockBanner';
 
+function Post() {
+  return (
+    <Container>
+      <SideMenu />
+      <Wrapper>
+        <StockBanner stock={bannerImg} />
+        <Form>
+          <p>Create Article</p>
+          <ArticleTitle type="text" name="title" placeholder="Title" />
+          <textarea name="address" placeholder="Content"></textarea>
+          <ArticleImg type="file" accept="image/*" multiple="multiple" />
+          <Button type="button" value="Submit" />
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+}
+const Container = styled.div`
+  display: flex;
+`;
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 70%;
+`;
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  width: 600px;
   justify-content: center;
   align-items: center;
   gap: 20px;
   margin: 80px auto;
-  padding: 20px;
+  padding: 40px;
   border-radius: 10px;
-  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+  border: 1px solid #c2c2c2;
+  &:hover {
+    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+  }
   input {
     padding: 5px;
     width: 100%;
@@ -38,19 +67,7 @@ const Button = styled.input`
   background-color: #373737;
   color: white;
   letter-spacing: 1.5px;
+  border-radius: 5px;
 `;
-const ArticleImg = styled.input`
-  border: none;
-`;
-function Post() {
-  return (
-    <Form>
-      <p>Create Article</p>
-      <ArticleTitle type="text" name="title" placeholder="Title" />
-      <textarea name="address" placeholder="Content"></textarea>
-      <ArticleImg type="file" accept="image/*" multiple="multiple" />
-      <Button type="button" value="Submit" />
-    </Form>
-  );
-}
+const ArticleImg = styled.input``;
 export default Post;
