a52e903e90a01bf28c0da3d719da15e669987fdd
wanhsuanlin <energy2429@gmail.com>
2023-03-21 00:10:00 +0800

    [Add] OrderPage's all data rendering

--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -150,25 +150,15 @@ const Checkbox = styled.input.attrs({ type: 'checkbox' })`
       border-radius: 2px;
     }
   }
-
-  /* & :checked {
-    pointer-events: none;
-    opacity: 0.5;
-  }
-
-  & :checked[selected] {
-    pointer-events: auto;
-    opacity: 1;
-  } */
 `;
 
 function Order() {
-  const [allOrders, setAllOrders] = useState(null);
-  const [establishedOrder, setEstablishedOrder] = useState(null);
-  const [pickUpGoods, setPickUpGoods] = useState(null);
-  const [delivery, setDelivery] = useState(null);
-  const [deliverComplete, setDeliverComplete] = useState(null);
-  const [completedOrder, setCompletedOrder] = useState(null);
+  const [allOrders, setAllOrders] = useState([]);
+  const [establishedOrder, setEstablishedOrder] = useState([]);
+  const [pickUpGoods, setPickUpGoods] = useState([]);
+  const [delivery, setDelivery] = useState([]);
+  const [deliverComplete, setDeliverComplete] = useState([]);
+  const [completedOrder, setCompletedOrder] = useState([]);
   const [selectedStatus, setSelectedStatus] = useState();
   const [editMode, setEditMode] = useState(false);
   const [page, setPage] = useState(1);
@@ -176,66 +166,52 @@ function Order() {
   // const [saveStatus, setSaveStatus] = useState('請先點擊 Edit 按鈕');
 
   useEffect(() => {
-    fetch(
-      `https://www.saiko.world/api/1.0/admin/newestOrder/allOrder?paging=${page}`
-    )
-      .then((res) => res.json())
-      .then((data) => setAllOrders(data));
-  }, [page]);
+    const fetchData = async () => {
+      try {
+        const urls = [
+          `https://www.saiko.world/api/1.0/admin/newestOrder/allOrder?paging=${page}`,
+          `https://www.saiko.world/api/1.0/admin/newestOrder/establishedOrder?paging=${page}`,
+          `https://www.saiko.world/api/1.0/admin/newestOrder/pickUpGoods?paging=${page}`,
+          `https://www.saiko.world/api/1.0/admin/newestOrder/delivery?paging=${page}`,
+          `https://www.saiko.world/api/1.0/admin/newestOrder/deliverComplete?paging=${page}`,
+          `https://www.saiko.world/api/1.0/admin/newestOrder/completedOrder?paging=${page}`,
+        ];
 
-  useEffect(() => {
-    fetch(
-      `https://www.saiko.world/api/1.0/admin/newestOrder/establishedOrder=${page}`
-    )
-      .then((res) => res.json())
-      .then((data) => setEstablishedOrder(data));
-  }, [page]);
+        const currentUrl = urls[selectedStatus];
+        const res = await fetch(currentUrl);
+        const result = await res.json();
+        const newData = result.data;
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res1 = await fetch(
-  //         `https://www.saiko.world/api/1.0/admin/newestOrder/allOrder?paging=${page}`
-  //       );
+        switch (selectedStatus) {
+          case '0':
+            setAllOrders(newData);
+            break;
+          case '1':
+            setEstablishedOrder(newData);
+            break;
+          case '2':
+            setPickUpGoods(newData);
+            break;
+          case '3':
+            setDelivery(newData);
+            break;
+          case '4':
+            setDeliverComplete(newData);
+            break;
+          case '5':
+            setCompletedOrder(newData);
+            break;
+          default:
+            console.error(`Invalid selectedStatus: ${selectedStatus}`);
+            break;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  //       const result1 = await res1.json();
-  //       setAllOrders(result1);
-
-  //       const res2 = await fetch(
-  //         `https://www.saiko.world/api/1.0/admin/newestOrder/establishedOrder=${page}`
-  //       );
-  //       const result2 = await res2.json();
-  //       setEstablishedOrder(result2);
-
-  //       const res3 = await fetch(
-  //         `https://www.saiko.world/api/1.0/admin/newestOrder/pickUpGoods=${page}`
-  //       );
-  //       const result3 = await res3.json();
-  //       setPickUpGoods(result3);
-
-  //       const res4 = await fetch(
-  //         `https://www.saiko.world/api/1.0/admin/newestOrder/delivery=${page}`
-  //       );
-  //       const result4 = await res4.json();
-  //       setDelivery(result4);
-
-  //       const res5 = await fetch(
-  //         `https://www.saiko.world/api/1.0/admin/newestOrder/deliverComplete${page}`
-  //       );
-  //       const result5 = await res5.json();
-  //       setDeliverComplete(result5);
-
-  //       const res6 = await fetch(
-  //         `https://www.saiko.world/api/1.0/admin/newestOrder/completedOrder=${page}`
-  //       );
-  //       const result6 = await res6.json();
-  //       setCompletedOrder(result6);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [page]);
+    fetchData();
+  }, [page, selectedStatus]);
 
   function handleNextPage() {
     setPage(page + 1);
@@ -246,87 +222,134 @@ function Order() {
   }
 
   function renderSelectedComponent() {
+    // if (allOrders === [] || '0') {
+    //   return (
+    //     <AllOrder>
+    //       <OrderStatus>訂單總覽</OrderStatus>
+    //       <OrderSection>
+    //         <OrderTitle />
+    //         <StockTableSection>
+    //           {console.log(allOrders)}
+    //           {/* {allOrders.data.map((order, index) => (
+    //             <StockTableItemGroup key={index}>
+    //               <StockTableItem>{order.order_id}</StockTableItem>
+    //               <StockTableItem>{order.recipient.name}</StockTableItem>
+    //               <StockTableItem>
+    //                 {order.recipient.phone}
+    //                 <br />
+    //                 {order.recipient.address}
+    //               </StockTableItem>
+    //               <StockTableItem>{order.recipient.email}</StockTableItem>
+    //               <StockTableItem>
+    //                 {order.details.list.map((item, index) => (
+    //                   <StockTableItemDetail>
+    //                     {`${index + 1}. ${item.name} / ${item.size} / ${
+    //                       item.qty
+    //                     }`}
+    //                   </StockTableItemDetail>
+    //                 ))}
+    //               </StockTableItem>
+    //               <StockTableItem>NT$ {order.total}</StockTableItem>
+    //               <StockTableItem>
+    //                 <Checkbox disabled={!editMode} key={index} />
+    //               </StockTableItem>
+    //             </StockTableItemGroup>
+    //           ))} */}
+    //         </StockTableSection>
+    //       </OrderSection>
+    //     </AllOrder>
+    //   );
+    // }
+
     switch (selectedStatus) {
-      case 'AllOrder':
+      case '0':
         return (
           <AllOrder>
             <OrderStatus>訂單總覽</OrderStatus>
             <OrderSection>
               <OrderTitle />
               <StockTableSection>
-                {allOrders.data.map((order, index) => (
-                  <StockTableItemGroup key={index}>
-                    <StockTableItem>{order.order_id}</StockTableItem>
-                    <StockTableItem>{order.recipient.name}</StockTableItem>
-                    <StockTableItem>
-                      {order.recipient.phone}
-                      <br />
-                      {order.recipient.address}
-                    </StockTableItem>
-                    <StockTableItem>{order.recipient.email}</StockTableItem>
-                    <StockTableItem>
-                      {order.details.list.map((item, index) => (
-                        <StockTableItemDetail>
-                          {`${index + 1}. ${item.name} / ${item.size} / ${
-                            item.qty
-                          }`}
-                        </StockTableItemDetail>
-                      ))}
-                    </StockTableItem>
-                    <StockTableItem>NT$ {order.total}</StockTableItem>
-                    <StockTableItem>
-                      <Checkbox disabled={!editMode} key={index} />
-                    </StockTableItem>
-                  </StockTableItemGroup>
-                ))}
+                {allOrders.length !== 0 ? (
+                  allOrders.map((order, index) => (
+                    <StockTableItemGroup key={index}>
+                      <StockTableItem>{order.order_id}</StockTableItem>
+                      <StockTableItem>{order.recipient.name}</StockTableItem>
+                      <StockTableItem>
+                        {order.recipient.phone}
+                        <br />
+                        {order.recipient.address}
+                      </StockTableItem>
+                      <StockTableItem>{order.recipient.email}</StockTableItem>
+                      <StockTableItem>
+                        {order.details.list.map((item, index) => (
+                          <StockTableItemDetail key={index}>
+                            {`${index + 1}. ${item.name} / ${item.size} / ${
+                              item.qty
+                            }`}
+                          </StockTableItemDetail>
+                        ))}
+                      </StockTableItem>
+                      <StockTableItem>NT$ {order.total}</StockTableItem>
+                      <StockTableItem>
+                        <Checkbox disabled={!editMode} key={index} />
+                      </StockTableItem>
+                    </StockTableItemGroup>
+                  ))
+                ) : (
+                  <div>loading</div>
+                )}
               </StockTableSection>
             </OrderSection>
           </AllOrder>
         );
-      case 'EstablishedOrder':
+      case '1':
         return (
           <EstablishedOrder>
             <OrderStatus>訂單已成立</OrderStatus>
             <OrderSection>
               <OrderTitle />
               <StockTableSection>
-                {establishedOrder.data.map((order, index) => (
-                  <StockTableItemGroup key={index}>
-                    <StockTableItem>{order.order_id}</StockTableItem>
-                    <StockTableItem>{order.recipient.name}</StockTableItem>
-                    <StockTableItem>
-                      {order.recipient.phone}
-                      <br />
-                      {order.recipient.address}
-                    </StockTableItem>
-                    <StockTableItem>{order.recipient.email}</StockTableItem>
-                    <StockTableItem>
-                      {order.details.list.map((item, index) => (
-                        <StockTableItemDetail>
-                          {`${index + 1}. ${item.name} / ${item.size} / ${
-                            item.qty
-                          }`}
-                        </StockTableItemDetail>
-                      ))}
-                    </StockTableItem>
-                    <StockTableItem>NT$ {order.total}</StockTableItem>
-                    <StockTableItem>
-                      <Checkbox disabled={!editMode} key={index} />
-                    </StockTableItem>
-                  </StockTableItemGroup>
-                ))}
+                {establishedOrder.length !== 0 ? (
+                  establishedOrder.map((order, index) => (
+                    <StockTableItemGroup key={index}>
+                      <StockTableItem>{order.order_id}</StockTableItem>
+                      <StockTableItem>{order.recipient.name}</StockTableItem>
+                      <StockTableItem>
+                        {order.recipient.phone}
+                        <br />
+                        {order.recipient.address}
+                      </StockTableItem>
+                      <StockTableItem>{order.recipient.email}</StockTableItem>
+                      <StockTableItem>
+                        {order.details.list.map((item, index) => (
+                          <StockTableItemDetail key={index}>
+                            {`${index + 1}. ${item.name} / ${item.size} / ${
+                              item.qty
+                            }`}
+                          </StockTableItemDetail>
+                        ))}
+                      </StockTableItem>
+                      <StockTableItem>NT$ {order.total}</StockTableItem>
+                      <StockTableItem>
+                        <Checkbox disabled={!editMode} key={index} />
+                      </StockTableItem>
+                    </StockTableItemGroup>
+                  ))
+                ) : (
+                  <div>loading</div>
+                )}
               </StockTableSection>
             </OrderSection>
           </EstablishedOrder>
         );
-      case 'PickUpGoods':
+      case '2':
         return (
           <PickUpGoods>
             <OrderStatus>揀貨中</OrderStatus>
             <OrderSection>
               <OrderTitle />
               <StockTableSection>
-                {pickUpGoods.data.map((order, index) => (
+                {pickUpGoods.map((order, index) => (
                   <StockTableItemGroup key={index}>
                     <StockTableItem>{order.order_id}</StockTableItem>
                     <StockTableItem>{order.recipient.name}</StockTableItem>
@@ -338,7 +361,7 @@ function Order() {
                     <StockTableItem>{order.recipient.email}</StockTableItem>
                     <StockTableItem>
                       {order.details.list.map((item, index) => (
-                        <StockTableItemDetail>
+                        <StockTableItemDetail key={index}>
                           {`${index + 1}. ${item.name} / ${item.size} / ${
                             item.qty
                           }`}
@@ -355,14 +378,14 @@ function Order() {
             </OrderSection>
           </PickUpGoods>
         );
-      case 'Delivery':
+      case '3':
         return (
           <Delivery>
             <OrderStatus>出貨中</OrderStatus>
             <OrderSection>
               <OrderTitle />
               <StockTableSection>
-                {delivery.data.map((order, index) => (
+                {delivery.map((order, index) => (
                   <StockTableItemGroup key={index}>
                     <StockTableItem>{order.order_id}</StockTableItem>
                     <StockTableItem>{order.recipient.name}</StockTableItem>
@@ -374,7 +397,7 @@ function Order() {
                     <StockTableItem>{order.recipient.email}</StockTableItem>
                     <StockTableItem>
                       {order.details.list.map((item, index) => (
-                        <StockTableItemDetail>
+                        <StockTableItemDetail key={index}>
                           {`${index + 1}. ${item.name} / ${item.size} / ${
                             item.qty
                           }`}
@@ -391,14 +414,14 @@ function Order() {
             </OrderSection>
           </Delivery>
         );
-      case 'DeliverComplete':
+      case '4':
         return (
           <DeliverComplete>
             <OrderStatus>已送達</OrderStatus>
             <OrderSection>
               <OrderTitle />
               <StockTableSection>
-                {deliverComplete.data.map((order, index) => (
+                {deliverComplete.map((order, index) => (
                   <StockTableItemGroup key={index}>
                     <StockTableItem>{order.order_id}</StockTableItem>
                     <StockTableItem>{order.recipient.name}</StockTableItem>
@@ -410,7 +433,7 @@ function Order() {
                     <StockTableItem>{order.recipient.email}</StockTableItem>
                     <StockTableItem>
                       {order.details.list.map((item, index) => (
-                        <StockTableItemDetail>
+                        <StockTableItemDetail key={index}>
                           {`${index + 1}. ${item.name} / ${item.size} / ${
                             item.qty
                           }`}
@@ -427,14 +450,14 @@ function Order() {
             </OrderSection>
           </DeliverComplete>
         );
-      case 'CompleteOrder':
+      case '5':
         return (
           <CompleteOrder>
             <OrderStatus>訂單已完成</OrderStatus>
             <OrderSection>
               <OrderTitle />
               <StockTableSection>
-                {completedOrder.data.map((order, index) => (
+                {completedOrder.map((order, index) => (
                   <StockTableItemGroup key={index}>
                     <StockTableItem>{order.order_id}</StockTableItem>
                     <StockTableItem>{order.recipient.name}</StockTableItem>
@@ -446,7 +469,7 @@ function Order() {
                     <StockTableItem>{order.recipient.email}</StockTableItem>
                     <StockTableItem>
                       {order.details.list.map((item, index) => (
-                        <StockTableItemDetail>
+                        <StockTableItemDetail key={index}>
                           {`${index + 1}. ${item.name} / ${item.size} / ${
                             item.qty
                           }`}
@@ -502,12 +525,12 @@ function Order() {
         />
         <StockStatusSection>
           <StockStatus onChange={handleChange}>
-            <StockOption value="AllOrder">訂單總覽</StockOption>
-            <StockOption value="EstablishedOrder">訂單已成立</StockOption>
-            <StockOption value="PickUpGoods">揀貨中</StockOption>
-            <StockOption value="Delivery">出貨中</StockOption>
-            <StockOption value="DeliverComplete">已送達</StockOption>
-            <StockOption value="CompleteOrder">訂單已完成</StockOption>
+            <StockOption value="0">訂單總覽</StockOption>
+            <StockOption value="1">訂單已成立</StockOption>
+            <StockOption value="2">揀貨中</StockOption>
+            <StockOption value="3">出貨中</StockOption>
+            <StockOption value="4">已送達</StockOption>
+            <StockOption value="5">訂單已完成</StockOption>
           </StockStatus>
           <StockListEdit onClick={handleEditClick}>Edit</StockListEdit>
           <StockListSave onClick={handleSaveClick}>Save</StockListSave>
